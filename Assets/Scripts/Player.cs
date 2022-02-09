8763f5dc56646b22754dffb22930625f9018b36c
29-Jude <jude77787@gmail.com>
2022-02-09 11:12:32 +0800

    Added Player Lose Condition
    - Player will lose when they collide with any of the obstacles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -28,6 +29,7 @@ public class Player : MonoBehaviour
         if(collision.gameObject.tag == "Obstacle")
         {
             Destroy(collision.gameObject);
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
