fce95996bb41f78fa359a80392b7fbf202bd1765
29-Jude <jude77787@gmail.com>
2022-02-09 10:34:04 +0800

    -Modified Spawners
    - Spawners now spawns multiple types of cubes with different colors

--- Assets/Scripts/Spawnerscript.cs
+++ Assets/Scripts/Spawnerscript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Spawnerscript : MonoBehaviour
 {
-    public GameObject SpawnObject;
+    public GameObject[] SpawnObject;
     float PositionY;
+    private int randomInt;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +24,9 @@ public class Spawnerscript : MonoBehaviour
 
     void SpawnObjects()
     {
+        randomInt = Random.Range(0, SpawnObject.Length);
         PositionY = Random.Range(4, -4f);
         this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
-        Instantiate(SpawnObject, transform.position, transform.rotation);
+        Instantiate(SpawnObject[randomInt], transform.position, transform.rotation);
     }
 }
